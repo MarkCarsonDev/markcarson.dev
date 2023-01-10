3c1ffbdab0da57247b264b82fc58156d5b46ed57
Mark <github@markcarson.dev>
2023-01-10 13:23:57 -0800

    Finalized cycling effect, added text for inconstruction page, reorganized layout.

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 window.onload = function () {
-    const textElement = document.getElementById("cycle2");
-    const textArray = JSON.parse(textElement.dataset.states);
-    let currentTextIndex = 0;
-    textElement.style.transform = "scaleY(0)";
 
-    textElement.addEventListener('transitionend', function(){
-        setTimeout(function(){
-            textElement.style.transform = "scaleY(0)";
-            textElement.style.transformOrigin = "top"
+	setTimeout(function () {
+		increment_text(document.getElementById("cycle"))
+	}, 3500);
 
-            currentTextIndex = currentTextIndex + 1 >= textArray.length ? 0 : currentTextIndex + 1;
-            textElement.textContent = textArray[currentTextIndex];
 
-            textElement.style.transform = "scaleY(1)";
-            textElement.style.transformOrigin = "bottom"
-        }, 2000)
-    });
+	function increment_text(t) {
 
+		let time = getTransitionDuration(t) * 1000;
+		let delay = parseFloat(t.dataset.delay)
+		const states = JSON.parse(t.dataset.states);
+
+		let currentTextIndex = states.indexOf(t.textContent);
+
+		t.style.transform = "scaleY(0)";
+		t.style.transformOrigin = "top";
+
+
+		setTimeout(function () {
+			currentTextIndex = (currentTextIndex + 1 >= states.length) ? 0 : currentTextIndex + 1;
+			t.textContent = states[currentTextIndex];
+
+			t.style.transform = "scaleY(1)";
+			t.style.transformOrigin = "bottom";
+		}, time)
+
+		setTimeout(function () {
+			increment_text(t)
+		}, delay)
+	}
+
+	function getTransitionDuration(element) {
+		return parseFloat(getComputedStyle(element).transitionDuration.substring(0, getComputedStyle(element).transitionDuration.indexOf("s")))
+	}
+
+	function getWidth(element) {
+		return parseFloat(getComputedStyle(element).width)
+	}
 
 }
