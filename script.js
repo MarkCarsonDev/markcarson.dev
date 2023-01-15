43f481fc7c1492b5b8bb7333aa741983bd3484b1
MarkCarsonDev <github@markcarson.dev>
2023-01-14 22:33:42 -0800

    Placeholders for portfolio page

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
 window.onload = function () {
 
+	pages = ["main", "portfolio"]
+
+	loadPage(pages[0]);
+
 	setTimeout(function () {
-		flip_text(document.getElementById("cycle"))
+		flip_text(document.getElementById("cycle"));
 	}, 3500);
+}
+
+function loadPage(page) {
+	pages.forEach(element => {
+		document.getElementById(element).classList.add("hidden");
+	});
+
+	document.getElementById(page).classList.remove("hidden");
+}
+
+function flip_text(t) {
+
+	let time = getTransitionDuration(t) * 1000;
+	let delay = parseFloat(t.dataset.delay);
+	const states = JSON.parse(t.dataset.states);
+
+	let currentTextIndex = states.indexOf(t.textContent);
+
+	t.style.transform = "scaleY(0)";
+	t.style.transformOrigin = "top";
 
 
-	function flip_text(t) {
+	setTimeout(function () {
+		currentTextIndex = (currentTextIndex + 1 >= states.length) ? 0 : currentTextIndex + 1;
+		t.textContent = states[currentTextIndex];
 
-		let time = getTransitionDuration(t) * 1000;
-		let delay = parseFloat(t.dataset.delay)
-		const states = JSON.parse(t.dataset.states);
+		t.style.transform = "scaleY(1)";
+		t.style.transformOrigin = "bottom";
+	}, time)
 
-		let currentTextIndex = states.indexOf(t.textContent);
+	setTimeout(function () {
+		flip_text(t);
+	}, delay)
+}
 
-		t.style.transform = "scaleY(0)";
-		t.style.transformOrigin = "top";
-
-
-		setTimeout(function () {
-			currentTextIndex = (currentTextIndex + 1 >= states.length) ? 0 : currentTextIndex + 1;
-			t.textContent = states[currentTextIndex];
-
-			t.style.transform = "scaleY(1)";
-			t.style.transformOrigin = "bottom";
-		}, time)
-
-		setTimeout(function () {
-			flip_text(t)
-		}, delay)
-	}
-
-	function getTransitionDuration(element) {
-		return parseFloat(getComputedStyle(element).transitionDuration.substring(0, getComputedStyle(element).transitionDuration.indexOf("s")))
-	}
-
+function getTransitionDuration(element) {
+	return parseFloat(getComputedStyle(element).transitionDuration.substring(0, 
+					getComputedStyle(element).transitionDuration.indexOf("s")));
 }
