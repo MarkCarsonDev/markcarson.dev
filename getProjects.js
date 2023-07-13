3a587a639d21960154cd21090d6e7f79da62ace6
MarkCarsonDev <64717642+MarkCarsonDev@users.noreply.github.com>
2023-07-13 10:58:52 +0200

    Added error handling to getProjects

--- getProjects.js
+++ getProjects.js
@@ -3,50 +3,57 @@ REPO_COUNT = 3
 function getTimeSince(date) {
     const seconds = Math.floor((new Date() - date) / 1000);
     let interval = seconds / 31536000;
-  
+
     if (interval > 1) {
-      return Math.floor(interval) + " years ago";
+        return Math.floor(interval) + " years ago";
     }
     interval = seconds / 2592000;
     if (interval > 2) {
-      return Math.floor(interval) + " months ago";
+        return Math.floor(interval) + " months ago";
     }
     interval = seconds / 86400;
     if (interval > 2) {
-      return Math.floor(interval) + " days ago";
+        return Math.floor(interval) + " days ago";
     }
     return "Today";
-  }
-  
-  async function getRecentProjects(user) {
+}
+
+async function getRecentProjects(user) {
     try {
-      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos`);
-      const repos = await repoResponse.json();
-  
-      // Sort the repositories by last push date in descending order
-      repos.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
-  
-      // Get the 4 most recently pushed repositories
-      let recentRepos = repos.slice(0, REPO_COUNT);
-  
-      const reposParent = document.getElementById(`projects-list`);
-      // Update the HTML
-      for (let i = 0; i < recentRepos.length; i++) {
-        const repo = recentRepos[i];
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos`);
+        const repos = await repoResponse.json();
 
-        let repoElement = document.createElement('li');
-  
-        const timeSince = getTimeSince(new Date(repo.pushed_at));
-        repoElement.innerHTML = `<a href="${repo.html_url}"><span class="underlined">${repo.name}</span> (${timeSince})</a>`;
+        const reposParent = document.getElementById(`projects-list`);
 
-        reposParent.appendChild(repoElement)
-      }
-      reposParent.removeChild(reposParent.getElementsByTagName('li')[0]);
-  
+        if (repoResponse.status === 403) {
+            reposParent.getElementsByTagName('li')[0].innerHTML = "Really? Rate limited? Just email me already...";
+            return;
+        }
+
+        // Sort the repositories by last push date in descending order
+        repos.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
+
+        // Get the 4 most recently pushed repositories
+        let recentRepos = repos.slice(0, REPO_COUNT);
+
+        // Update the HTML
+        reposParent.removeChild(reposParent.getElementsByTagName('li')[0]);
+        for (let i = 0; i < recentRepos.length; i++) {
+            const repo = recentRepos[i];
+
+            let repoElement = document.createElement('li');
+
+            const timeSince = getTimeSince(new Date(repo.pushed_at));
+            repoElement.innerHTML = `<a href="${repo.html_url}"><span class="underlined">${repo.name}</span> (${timeSince})</a>`;
+
+            reposParent.appendChild(repoElement)
+        }
+
     } catch (error) {
-      console.error(error);
+        console.error(error);
+        const reposParent = document.getElementById(`projects-list`);
+        reposParent.getElementsByTagName('li')[0].innerHTML = "Uh oh! Something made a fucky wucky! Sowwy! ;-;";
     }
-  }
-  
-  getRecentProjects('MarkCarsonDev');
-  
+}
+
+getRecentProjects('MarkCarsonDev');
